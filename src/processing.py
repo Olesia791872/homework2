7778ad64b81423f2dbb6e72844a4786c33ff3db2
Olesia Kirillova <olesia89824982142@gmail.com>
2024-10-11 04:23:53 -0700

    new_def

--- src/processing.py
+++ src/processing.py
@@ -1,38 +1,33 @@
 from typing import List, Dict, Optional
 
-def filter_by_state(data: List[Dict], state: Optional[str] = 'EXECUTED') -> List[Dict]:
- """
- Фильтрация списка словарей по ключу "state".
 
- Args:
-  data: Список словарей.
-  state: Значение ключа "state" для фильтрации. По умолчанию 'EXECUTED'.
+def filter_by_state(data: List[Dict], state: Optional[str] = "EXECUTED") -> List[Dict]:
+    """
+    Фильтрация списка словарей по ключу "state".
 
- Returns:
-  Новый список словарей, содержащий только словари, у которых ключ "state"
-  соответствует указанному значению.
- """
+    Args:
+     data: Список словарей.
+     state: Значение ключа "state" для фильтрации. По умолчанию "EXECUTED".
 
- filtered_data = []
- for item in data:
-  if item.get("state") == state:
-   filtered_data.append(item)
- return filtered_data
+    Returns:
+     Новый список словарей, содержащий только словари, у которых ключ "state"
+     соответствует указанному значению.
+    """
 
+    filtered_data = []
+    for item in data:
+        if item.get("state") == state:
+            filtered_data.append(item)
+    return filtered_data
 
-
-
-Пример использования:
 
 data = [
-  {'id': 1, 'state': 'EXECUTED'},
-  {'id': 2, 'state': 'PENDING'},
-  {'id': 3, 'state': 'EXECUTED'},
-  {'id': 4, 'state': 'CANCELED'},
+    {"id": 1, "state": "EXECUTED"},
+    {"id": 2, "state": "PENDING"},
+    {"id": 3, "state": "EXECUTED"},
+    {"id": 4, "state": "CANCELED"},
 ]
 
-filtered_data = filter_by_state(data, 'EXECUTED')
+filtered_data = filter_by_state(data, "EXECUTED")
 
 print(filtered_data)
-
-
