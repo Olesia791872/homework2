dd14b8b5703595978a633b28063ea31ed1690af0
Olesia Kirillova <olesia89824982142@gmail.com>
2024-10-11 04:35:48 -0700

    new_def

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import Dict, List, Optional
 
 
 def filter_by_state(data: List[Dict], state: Optional[str] = "EXECUTED") -> List[Dict]:
@@ -31,3 +31,36 @@ data = [
 filtered_data = filter_by_state(data, "EXECUTED")
 
 print(filtered_data)
+
+
+from typing import Dict, List, Optional
+
+
+def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+    Сортировка списка словарей по дате (ключ "date").
+
+    Args:
+     data: Список словарей.
+     reverse: Флаг, указывающий порядок сортировки. True - по убыванию (по умолчанию), False - по возрастанию.
+
+    Returns:
+     Новый список словарей, отсортированный по дате.
+    """
+    return sorted(data, key=lambda item: item["date"], reverse=reverse)
+
+
+data = [
+    {"id": 1, "date": "2023-03-10"},
+    {"id": 2, "date": "2023-03-05"},
+    {"id": 3, "date": "2023-03-15"},
+]
+
+sorted_data = sort_by_date(data)
+
+print(sorted_data)
+
+
+sorted_data = sort_by_date(data, reverse=False)
+
+print(sorted_data)
